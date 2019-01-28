a6dbbd271da965df52554e97aa0e42d2eee9c443
Michael Houmann <mhoumann87@gmail.com>
2019-01-28 11:48:40 +0100

    Remove bootstrap, add own styles, add weather svgs

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,96 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <header>
       <a class="navbar-brand" href="#/">Weather App</a>
-    </nav>
-    <main class="container mt-3">
+    </header>
+    <main>
       <router-view/>
     </main>
+    <footer>
+      <div class="copy">
+        <p>&copy; 2019 Made by Michael Houmann inspired by the Coding Garden</p>
+      </div>
+      <div class="dark-sky">
+        <a href="https://darksky.net/poweredby/" target="_blank">Powered by Dark Sky</a>
+      </div>
+    </footer>
   </div>
 </template>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+}
+html {
+  box-sizing: border-box;
+}
+
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  background: whitesmoke;
+  font-size: 100%;
+}
+
+p,
+a {
+  font-family: "Cardo", serif;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Sarabun", sans-serif;
+  font-weight: 100;
+}
+
+header {
+  width: 100%;
+  padding: 1rem;
+  background-color: #78c2ad;
+  box-shadow: 2px 2px 0 rgba(53, 53, 53, 0.5);
+  position: fixed;
+}
+
+header a,
+header a:visited,
+header a:hover,
+header a:active,
+header a:focus {
+  font-family: "Sarabun", sans-serif;
+  font-size: 2em;
+  color: white;
+  font-weight: 100;
+  text-decoration: none;
+  margin: 0;
+}
+main {
+  min-height: 90vh;
+}
+
+footer {
+  background-color: #78c2ad;
+
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  color: white;
+}
+
+footer a,
+footer a:visited,
+footer a:hover,
+footer a:active,
+footer a:focus {
+  font-family: "Sarabun", sans-serif;
+  color: white;
+  font-weight: 600;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,78 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <div class="row mt-3">
-      <div class="card text-white bg-primary col-3">
-        <div class="card-header">Location</div>
-        <div class="card-body">
-          <h4 class="card-title">{{forecast.timezone}}</h4>
-          <p
-            class="card-text"
-          >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <section>
+    <div class="home">
+      <div class="box">
+        <div class="box__header">
+          <h2>Location</h2>
         </div>
-      </div>
-      <div class="card text-white bg-primary col-3 ml-5">
-        <div class="card-header">Current Weather</div>
-        <div class="card-body">
-          <div class="icon-box">
-            <h4 class="card-title">
-              <i class="fas fa-sun"></i>
-            </h4>
+        <div class="box__content">
+          <div>
+            <p class="space">The weather for:</p>
+            <p>{{forecast.timezone}}</p>
           </div>
-          <p class="card-text">{{forecast.currently.summary}}</p>
-          <p>Temprature: {{forecast.currently.temperature}}</p>
+          <div class="search">
+            <input type="text" name="search" id="search" placeholder="Find a city">
+            <button type="submit">Find</button>
+          </div>
         </div>
       </div>
+      <div class="box">
+        <div class="box__header">
+          <h2>Current Weather</h2>
+        </div>
+        <div class="box__content">
+          <div class="icon">
+            <img :src="icons[forecast.currently.icon]">
+          </div>
+          <p>Weather right now: {{forecast.currently.summary}}</p>
+          <p>Temperature: {{forecast.currently.temperature}}</p>
+          <p>Wind speed: {{forecast.currently.windSpeed}} km/h</p>
+        </div>
+      </div>
+      <div class="box">
+        <div class="box__header">
+          <h2>Forecast</h2>
+        </div>
+        <div class="box__content">
+          <p class="space">The next few days:</p>
+          <p>{{forecast.daily.summary}}</p>
+        </div>
+      </div>
+      <pre>{{forecast}}</pre>
     </div>
-    <pre>{{forecast}}</pre>
-  </div>
+  </section>
 </template>
 
 <script>
 import API from "@/lib/API";
+import clearDay from "@/assets/images/clear-day.svg";
+import clearNight from "@/assets/images/clear-night.svg";
+import rain from "@/assets/images/rain.svg";
+import snow from "@/assets/images/snow.svg";
+import sleet from "@/assets/images/sleet.svg";
+import wind from "@/assets/images/wind.svg";
+import fog from "@/assets/images/fog.svg";
+import cloudy from "@/assets/images/cloudy.svg";
+import partlyCloudyDay from "@/assets/images/partly-cloudy-day.svg";
+import partlyCloudyNight from "@/assets/images/partly-cloudy-night.svg";
 
 export default {
   name: "home",
   data() {
     return {
-      forecast: {}
+      forecast: {},
+      icons: {
+        "clear-day": clearDay,
+        "clear-night": clearNight,
+        rain: rain,
+        snow: snow,
+        sleet: sleet,
+        wind: wind,
+        fog: fog,
+        cloudy: cloudy,
+        "partly-cloudy-day": partlyCloudyDay,
+        "partly-cloudy-night": partlyCloudyNight
+      }
     };
   },
   mounted() {
@@ -47,14 +84,82 @@ export default {
 </script>
 
 <style scoped>
-.icon-box {
-  width: 100%;
-  display: flex;
-  justify-content: center;
+.home {
+  padding: 8em 4em 2em;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
 }
 
-i {
-  font-size: 6rem;
+.box {
+  background-color: #78c2ad;
+  color: white;
+  border-radius: 5px;
+  box-shadow: 1px 1px 0 2px rgba(53, 53, 53, 0.2);
+}
+
+.box__header {
+  background-color: #74bca8;
+  width: 100%;
+  padding: 0.5em 0;
+  font-size: 1.4em;
+  text-align: center;
+  border-radius: 5px 5px 0 0;
+  margin-bottom: 0.7em;
+  box-shadow: 0 1px 0 0 rgba(53, 53, 53, 0.3);
+}
+
+.box__content {
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  font-size: 1.2rem;
+}
+
+.icon {
+  width: 50%;
+  margin: 0 auto 0.5em;
+}
+.img {
   display: block;
+  width: 100%;
+}
+
+.space {
+  margin-bottom: 0.5em;
+}
+
+.search {
+  margin-top: 4rem;
+  display: flex;
+  align-items: center;
+}
+
+.search input {
+  flex: 1;
+  padding: 0.5rem;
+  font-size: 1.2rem;
+  border: none;
+  background: #78c2ad;
+  border-bottom: 2px solid white;
+  outline: none;
+}
+
+.search input::placeholder {
+  color: white;
+}
+
+.search button {
+  margin-left: 0.5em;
+  padding: 0.5em;
+  font-size: 1em;
+  border: 2px solid white;
+  color: white;
+  background: #78c2ad;
+  outline: none;
+}
+
+pre {
+  grid-column: 1 / -1;
 }
 </style>
